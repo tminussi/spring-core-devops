910de67aad5672d847593a973c2c1f212ad3bf92
Thales Minussi <thales@MacBook-Air-de-Thales.local>
2017-06-01 21:39:44 -0300

    should trigger jenkins build

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,7 +9,7 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		//this is a whitespace change test
-		// another test...
+		System.out.println("Should trigger a jenkins build");
+
 	}
 }
