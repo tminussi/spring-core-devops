e72059dcd8171e25001044b0cf03de6e52f124e6
Thales Minussi <thales@MacBook-Air-de-Thales.local>
2017-06-01 21:51:11 -0300

    should trigger jenkins build

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,7 +9,7 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		System.out.println("Should trigger jenkins build - again");
+		System.out.println("Should trigger jenkins build - again - I think it was a problem with AWS Security Groups");
 
 	}
 }
